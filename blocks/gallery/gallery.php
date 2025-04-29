1bf98797c78c01d0bd23f89aead260be7c835d74
pythedev <pyork@ihg.co.uk>
2025-04-29 18:44:04 +0100

    v2.2.5

--- blocks/gallery/gallery.php
+++ blocks/gallery/gallery.php
@@ -35,45 +35,44 @@ $styles = array( 'background-color: ' . $background_color, 'color: ' . $text_col
 $style  = implode( '; ', $styles );
 ?>
 
-<div <?php echo esc_attr( $anchor ); ?>class="<?php echo esc_attr( $class_name ); ?>" style="<?php echo esc_attr( $style ); ?>">
+<div <?php echo esc_attr( $anchor ); ?>class="<?php echo esc_attr( $class_name ); ?>" >
    <div class="container">
 <?php
 $photo_shares = get_field('the_photo_share_bef');
 if( $photo_shares ): ?>
     <div class="cards gallery-ps-grid justify-content-center  row row-cols-lg-3 row-cols-md-2 row-cols-1 gap-2">
-    <?php foreach( $photo_shares as $photo_share ): 
-        $permalink = get_permalink( $photo_share->ID );
-        $title = get_the_title( $photo_share->ID );
-        $featured_photo_img = get_the_post_thumbnail_url($photo_share->ID);
-        $ps_event_date = get_field( 'ps_event_date', $photo_share->ID );
-        $ps_event_location = get_field( 'ps_event_location', $photo_share->ID );
-        $custom_field = get_field( 'field_name', $photo_share->ID );
-        ?>
-        <div class="card col m-bottom--3">
-                <a class="post-card post-card--small p-bottom--3" href="<?php echo esc_url( $permalink ); ?>">
-            <?php if ( has_post_thumbnail($photo_share->ID) ) { ?>
-                <img src="<?php echo esc_url($featured_photo_img); ?>" class="card-img m-bottom--1" alt="..bjb.">
-            <?php } ?>
-            <div class="post-card__content-container">
-                  <h3 class="post-card__title m-bottom--1"><?php echo esc_html( $title ); ?></h3>
-        <div class="post-card__content-row p-top--2 post-card__content-meta-row">
-              <div class="post-card__tags">
-              <?php if ( $ps_event_date ) : ?>
-            <p class="post-card__date post-card__tag"><?php echo esc_html( $ps_event_date ); ?></p>
-        <?php endif; ?>
-  <?php if ( $ps_event_location ) : ?>
-            <p class="post-card__date post-card__tag"><?php echo esc_html( $ps_event_location ); ?></p>
-        <?php endif; ?>
-            <p class="view post-card__location post-card__category">View Gallery</p>
+        <?php foreach( $photo_shares as $photo_share ): 
+            $permalink = get_permalink( $photo_share->ID );
+            $title = get_the_title( $photo_share->ID );
+            $featured_photo_img = get_the_post_thumbnail_url($photo_share->ID);
+            $ps_event_date = get_field( 'ps_event_date', $photo_share->ID );
+            $ps_event_location = get_field( 'ps_event_location', $photo_share->ID );
+            $custom_field = get_field( 'field_name', $photo_share->ID );
+            ?>
+                <div class="card col m-bottom--3">
+                    <a class="post-card post-card--small p-bottom--3" href="<?php echo esc_url( $permalink ); ?>">
+                        <?php if ( has_post_thumbnail($photo_share->ID) ) { ?>
+                            <img src="<?php echo esc_url($featured_photo_img); ?>" class="card-img m-bottom--1" alt="..bjb.">
+                        <?php } ?>
+                        <div class="post-card__content-container">
+                            <h3 class="post-card__title m-bottom--1"><?php echo esc_html( $title ); ?></h3>
+                            <div class="post-card__content-row p-top--2 post-card__content-meta-row">
+                                <div class="post-card__tags">
+                                    <?php if ( $ps_event_date ) : ?>
+                                        <p class="post-card__date post-card__tag"><?php echo esc_html( $ps_event_date ); ?></p>
+                                    <?php endif; ?>
+                                    <?php if ( $ps_event_location ) : ?>
+                                        <p class="post-card__date post-card__tag"><?php echo esc_html( $ps_event_location ); ?></p>
+                                    <?php endif; ?>
+
+                                    <p class="view post-card__location post-card__category">View Gallery</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
-            </a>
-        </div>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
     </div>
 <?php endif; ?>
 </div>
-
- 
 </div>
