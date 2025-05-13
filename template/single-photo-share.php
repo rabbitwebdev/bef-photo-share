82a45c886f2b37deb029e02bfc179355c301061a
pythedev <pyork@ihg.co.uk>
2025-05-13 19:44:46 +0100

    v3.4.0

--- template/single-photo-share.php
+++ template/single-photo-share.php
@@ -46,7 +46,7 @@ get_header(); ?>
 $photo_gallerys = get_field('single_bef_the_gallery');
 if( $photo_gallerys ): ?>
   
-        <div class="the_gallery row row-cols-lg-4 row-cols-md-2 row-cols-1 gap-2">
+        <div class="the_gallery row row-cols-lg-3 row-cols-md-2 row-cols-1 gap-2">
              <?php  $i = 1; ?>
             <?php foreach( $photo_gallerys as $photo_gallery ): ?>
                 <?php   $image_caption = $photo_gallery['caption']; ?>
